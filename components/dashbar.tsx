aaaa3644889ab6d9793793f7518db09818c46a4e
“Bilegt6969” <bilegt6969@gmail.com>
2023-06-07 21:42:37 +0800

    day 11

--- components/dashbar.tsx
+++ components/dashbar.tsx
@@ -1,10 +1,5 @@
-'use client'
 import React from 'react';
 import { Roboto } from 'next/font/google';
-import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
-import { useState } from 'react'
-
- 
 
 const roboto = Roboto({
   weight: '400',
@@ -13,21 +8,8 @@ const roboto = Roboto({
  
 
 function Dashbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
-  const [isOpen1, setIsOpen1] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen1(!isOpen1);
-  };
-
 return (
-  
-  <div className="sticky z-[-1] top-0 overflow-y-scroll ">
-    
-    
-
-    <div className="col-span-1 mt-8 hidden sm:hidden md:flex lg:flex  ">
+  <div className=" col-span-1 mt-8 hidden sm:hidden md:flex lg:flex ">
   <div className={roboto.className}>
     <h1 className="text-lg font-bold mb-4 text-white">HTML tutorial</h1>
     <div className="pl-4 text-[#9ba1a6] cursor-pointer flex flex-col space-y-3">
@@ -62,11 +44,9 @@ return (
       <a href='../html-' className="hover:text-gray-100 transition duration-100 ease-in-out">HTML Input Type</a>
       <a href='../html-' className="hover:text-gray-100 transition duration-100 ease-in-out">HTML Submit Button</a>
     </div>
-    </div>
-
+    
+  </div>
 </div>
-</div>
-
 
 
 );
