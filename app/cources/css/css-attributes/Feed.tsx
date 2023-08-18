1ab14acbb38b56de0f0e52413d51f4c5611403f0
“Bilegt6969” <bilegt6969@gmail.com>
2023-08-18 17:45:30 +0800

    versionZeron

--- app/cources/css/css-attributes/Feed.tsx
+++ app/cources/css/css-attributes/Feed.tsx
@@ -14,14 +14,14 @@ function DataComponent() {
  сонгогч нь тухайн атрибут буюу шинж чанар бүхий элементийг сонгоход ашигладаг.Дараах жишээ нь бүх <span className="text-pink-500">&lt;a&gt;</span> элементээс target шинж чанарыг агуулсан элементийг сонгох болно.</p>
     <h3 className="font-bold text-4xl mb-4 mt-8">Жишээ нь</h3>
     <div dangerouslySetInnerHTML={{ __html: collectedData[0].Url }} />
-    <h3 className="font-bold text-4xl mb-4 mt-8">CSS [атрибут="утга"] сонгогч</h3>
+    <h3 className="font-bold text-4xl mb-4 mt-8">CSS [атрибут=&quot;утга&quot;] сонгогч</h3>
     <p className="mb-4 mt-4">    <code className="mb-4 mt-4"><code className="text-pink-500">[атрибут="утга"] </code></code>
  сонгогч нь тодорхой шинж чанар, утгатай элементийг сонгоход ашигладаг.Дараах жишээ нь бүх <span className="text-pink-500">&lt;a&gt;</span> элементээс target шинж чанарыг агуулсан элементийг сонгох болно.</p>
  <h3 className="font-bold text-4xl mb-4 mt-8">Жишээ нь</h3>
     <div dangerouslySetInnerHTML={{ __html: collectedData[1].Url }} />
-    <h3 className="font-bold text-4xl mb-4 mt-8">CSS [атрибут~="утга"] сонгогч</h3>
-    <p className="mb-4 mt-4">    <code className="mb-4 mt-4"><code className="text-pink-500">[атрибут~="утга"] </code></code>
- нь тухайн үгийг агуулсан атрибутын утгатай элементийг сонгоход ашигладаг.Дараах жишээ нь бүх элементийн title шинж чанарт "mountain" гэсэн дан, эсвэл хоосон зайгаар тусгаарласан утгатай элементүүдэд хамаарах болно.</p>
+    <h3 className="font-bold text-4xl mb-4 mt-8">CSS [атрибут~=&quot;утга&quot;] сонгогч</h3>
+    <p className="mb-4 mt-4">    <code className="mb-4 mt-4"><code className="text-pink-500">[атрибут~=&quot;утга&quot;] </code></code>
+ нь тухайн үгийг агуулсан атрибутын утгатай элементийг сонгоход ашигладаг.Дараах жишээ нь бүх элементийн title шинж чанарт &quot;mountain&quot; гэсэн дан, эсвэл хоосон зайгаар тусгаарласан утгатай элементүүдэд хамаарах болно.</p>
     <h3 className="font-bold text-4xl mb-4 mt-8">Жишээ нь</h3>
     <div dangerouslySetInnerHTML={{ __html: collectedData[2].Url }} />
     
--- app/cources/css/css-icon/Feed.tsx
+++ app/cources/css/css-icon/Feed.tsx
@@ -13,17 +13,17 @@ function DataComponent() {
     <li className="mb-4 mt-4">Inline HTML элементийн class-т тухайн дүрсийн /icon/-н нэрийг зааж өгнө. /<span className="text-pink-500">&lt;i&gt;</span> эсвэл <span className="text-pink-500">&lt;span&gt;</span>/</li>
     <li className="mb-4 mt-4">Бүх дүрсийн хэмжээг өөрчлөх, өнгө өгөх, сүүдэр өгөх зэрэг боломжтой.</li>
     <h3 className="font-bold text-4xl mb-4 mt-8">Font Awesome Icons</h3>
-    <p className="mb-4 mt-4">Font Awesome Icons-г ашиглахын тулд fontawesome.com сайтад бүртгүүлж код авна. Авсан кодоо өөрийн HTML хуудасны <span className="text-pink-500">&lt;head&gt;</span> section буюу хэсэгт нэмж өгнө. &lt;script src="https://kit.fontawesome.com/tanii_code.js"&gt;<span className="text-pink-500">&lt;/script&gt;</span></p>
+    <p className="mb-4 mt-4">Font Awesome Icons-г ашиглахын тулд fontawesome.com сайтад бүртгүүлж код авна. Авсан кодоо өөрийн HTML хуудасны <span className="text-pink-500">&lt;head&gt;</span> section буюу хэсэгт нэмж өгнө. &lt;script src=&quot;https://kit.fontawesome.com/tanii_code.js&quot;&gt;<span className="text-pink-500">&lt;/script&gt;</span></p>
     <h3 className="font-bold text-4xl mb-4 mt-8">Жишээ нь</h3>
     <div dangerouslySetInnerHTML={{ __html: collectedData[0].Url }} />
     <h3 className="font-bold text-4xl mb-4 mt-8">Bootstrap Icons</h3>
     <p className="mb-4 mt-4">Bootstrap glyphicons-ийг ашиглахын тулд HTML хуудасны <span className="text-pink-500">&lt;head&gt;</span> хэсэгт доорох мөрийг нэмж өгнө.</p>
-    <p className="mb-4 mt-4">&lt;link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"&gt;</p>
+    <p className="mb-4 mt-4">&lt;link rel=&quot;stylesheet&quot; href=&quot;https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css&quot;&gt;</p>
     <h3 className="font-bold text-4xl mb-4 mt-8">Жишээ нь</h3>
     <div dangerouslySetInnerHTML={{ __html: collectedData[1].Url }} />
     <h3 className="font-bold text-4xl mb-4 mt-8">Google Icons</h3>
     
-    <p className="mb-4 mt-4">Google icons-ийг ашиглахын тулд HTML хуудасны <span className="text-pink-500">&lt;head&gt;</span> хэсэгт доорох мөрийг нэмнэ үү. &lt;link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons""&gt;</p>
+    <p className="mb-4 mt-4">Google icons-ийг ашиглахын тулд HTML хуудасны <span className="text-pink-500">&lt;head&gt;</span> хэсэгт доорох мөрийг нэмнэ үү. &lt;link rel=&quot;stylesheet&quot; href=&quot;https://fonts.googleapis.com/icon?family=Material+Icons&quot;&gt;</p>
     <h3 className="font-bold text-4xl mb-4 mt-8">Жишээ нь</h3>
     <div dangerouslySetInnerHTML={{ __html: collectedData[2].Url }} />
     <p className="mb-4 mt-4"></p>
